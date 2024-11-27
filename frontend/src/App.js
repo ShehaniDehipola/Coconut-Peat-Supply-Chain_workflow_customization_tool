95a919cef704d15ccda6336140b733c9a116f8f1
Shehani Dehipola <shehanidehipola@gmail.com>
2024-11-28 04:57:05 +0530

    added login feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import WorkflowCustomizationTool from './components/WorkflowCustomizationTool';
 import AddNewPluginPage from './components/AddNewPluginPage';
+import Navbar from './components/Navbar';
+import Login from './components/user/Login';
 
 function App() {
+  const location = useLocation(); // Get current route location
+
+  const showNavbar = location.pathname !== "/login"; // Show Navbar only if not on Login page
+
   return (
-    <Router>
+    <div>
+      {showNavbar && <Navbar />} {/* Conditionally render Navbar */}
       <Routes>
         {/* Route for the Workflow Customization Tool */}
         <Route path="/" element={<WorkflowCustomizationTool />} />
 
+        {/* Route for the Login page */}
+        <Route path="/login" element={<Login />} />
+
         {/* Route for the Add New Plugin page */}
         <Route path="/add-plugin" element={<AddNewPluginPage />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
-export default App;
+export default function Root() {
+    return (
+        <Router>
+            <App />
+        </Router>
+    );
+}
+
