00f7cab855996025c5fd1b0f2fb97407e0092132
Shehani Dehipola <shehanidehipola@gmail.com>
2024-12-03 04:13:15 +0530

    add route paths and config files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import WorkflowCustomizationTool from './components/WorkflowCustomizationTool';
 import AddNewPluginPage from './components/AddNewPluginPage';
 import Navbar from './components/Navbar';
 import Login from './components/user/Login';
+import { WorkflowOverview } from './components/exporter/WorkflowOverview';
+import { WorkflowProgress } from './components/exporter/WorkflowProgress';
+import { ManufacturerDashboard } from './components/manufacturer/ManufacturerDashboard';
+import { ManufacturerWorkflowPage } from './components/manufacturer/ManufaturerWorkflowPage';
 
 function App() {
   const location = useLocation(); // Get current route location
@@ -22,6 +26,18 @@ function App() {
 
         {/* Route for the Add New Plugin page */}
         <Route path="/add-plugin" element={<AddNewPluginPage />} />
+
+         {/* Route for the exporter workflow page */}
+        <Route path="/workflow" element={<WorkflowOverview />} />
+
+         {/* Route for the exporter workflow track page */}
+        <Route path="/progress" element={<WorkflowProgress />} />
+
+         {/* Route for the manufacturer dashboard */}
+        <Route path="/manufacturer-dashboard" element={<ManufacturerDashboard />} />
+
+        {/* Route for the manufacturer workflow  */}
+        <Route path="/manufacturer-workflow" element={<ManufacturerWorkflowPage />} />
       </Routes>
     </div>
   );
